0cfd0cb74793dcbf2917bb1b9f43bf915955be59
arthur böök <atte.book@gmail.com>
2023-10-29 16:23:49 -0700

    factory registry base class

--- src/configmate/base.py
+++ src/configmate/base.py
@@ -1,25 +1,24 @@
 import abc
 import collections
-import functools
-from typing import Any, Callable, Generic, NoReturn, OrderedDict, TypeVar
+from typing import Any, Callable, Deque, Generic, NoReturn, Tuple, TypeVar, cast
 
 from configmate import exceptions
 
 T = TypeVar("T")
 U = TypeVar("U")
+V = TypeVar("V")
 
 
-class ConfigMateVerbosity:
-    def __call__(self) -> int:
-        return 0
+class Description:
+    def __get__(self, instance: "HasDescription", _: Any) -> str:
+        return instance.__doc__ or ""
+
+    def __set__(self, instance: "HasDescription", value: str) -> None:
+        instance.__doc__ = value
 
 
 class HasDescription(abc.ABC):
-    def describe(self, verbosity: int) -> str:
-        return repr(self) if verbosity else self.short_description()
-
-    def short_description(self) -> str:
-        return ""
+    description = Description()
 
 
 class BaseSource(HasDescription, abc.ABC, Generic[T]):
@@ -28,44 +27,56 @@ class BaseSource(HasDescription, abc.ABC, Generic[T]):
         ...
 
 
-class BaseFactory(abc.ABC, Generic[T, U]):
+class BaseInterpolator(HasDescription, abc.ABC):
     @abc.abstractmethod
-    def __call__(self, *input_args: T) -> U:
+    def interpolate(self, text: str) -> str:
         ...
 
 
-class BaseRegistry(abc.ABC, Generic[T, U]):
-    __registry__: OrderedDict[T, U]
+class BaseParser(HasDescription, abc.ABC):
+    @abc.abstractmethod
+    def parse(self, configlike: str) -> Any:
+        ...
+
+
+class BaseValidator(HasDescription, abc.ABC, Generic[T]):
+    @abc.abstractmethod
+    def validate(self, object_: Any) -> T:
+        ...
+
+
+class BaseRegistry(HasDescription, abc.ABC, Generic[T, U]):
+    _registry: Deque[Tuple[Callable[[T], bool], U]]
+    _sentinel = object()
 
     def __init_subclass__(cls, *args, **kwargs) -> None:
-        cls.__registry__ = collections.OrderedDict()
+        cls._registry = collections.deque()
 
-    @staticmethod
-    @abc.abstractmethod
-    def is_valid_strategy(type_: Any, key: T) -> bool:
-        ...
+    def __getitem__(self, key: T) -> U:
+        return self.get_strategy(key)
 
     @classmethod
-    def get_strategy(cls, key: Any) -> U:
-        is_valid = functools.partial(cls.is_valid_strategy, key)
-        valid_keys = filter(is_valid, reversed(cls.__registry__.keys()))
-        try:
-            return cls.__registry__[next(valid_keys)]
-        except StopIteration as exc:
-            cls.raise_missing(key, exc)
+    def get_strategy(cls, key: T) -> U:
+        strategies = (strategy for checker, strategy in cls._registry if checker(key))
+        if (first_match := next(strategies, cls._sentinel)) is cls._sentinel:
+            cls.raise_missing(key)
+        return cast(U, first_match)
 
     @classmethod
-    def register(cls, *keys: T) -> Callable[[U], U]:
-        return lambda strategy: cls.update(strategy, *keys) or strategy
+    def register(cls, trigger: Callable[[T], bool], rank: int = -1) -> Callable[[U], U]:
+        return lambda strategy: cls.add(trigger, strategy, rank) or strategy
 
     @classmethod
-    def update(cls, strategy: U, *keys: T, update_order: bool = False) -> None:
-        _ = [cls.__registry__.pop(key, None) for key in keys if update_order]
-        cls.__registry__.update({key: strategy for key in keys})
+    def add(cls, trigger: Callable[[T], bool], strategy: U, rank: int = -1) -> None:
+        cls._registry.insert(rank, (trigger, strategy))
 
     @classmethod
-    def raise_missing(cls, unfound_key: T, cause: Exception) -> NoReturn:
+    def raise_missing(cls, unfound_key: Any) -> NoReturn:
         raise exceptions.NoStrategyAvailable(
             f"Missing strategy for {unfound_key}, please register one."
-            f"\n\tAvailable strategies: {list(cls.__registry__.keys())}"
-        ) from cause
+            f"\n\tAvailable strategies: "  # TODO use describe() here
+        )
+
+
+class BaseFactoryRegistry(BaseRegistry[T, Callable[[T], U]]):
+    """Holds factory methods"""
