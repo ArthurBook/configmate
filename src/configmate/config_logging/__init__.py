db71fba0b572c05c7f2d84fb8b2611cd98258a95
Arthur <atte.book@gmail.com>
2023-11-25 09:45:32 -0800

    deleted config logging

--- src/configmate/config_logging/__init__.py
+++
@@ -1,9 +0,0 @@
-from configmate.config_logging.configmate_logger import (
-    LoggingLevels,
-    logger,
-)
-from configmate.config_logging.cli_setup import (
-    set_config_from_arg_argparser,
-    set_verbosity_level,
-    set_log_dir,
-)
