83a0f35b32de3ab8b61c3fd48860393f65cad6be
Arthur <atte.book@gmail.com>
2023-11-25 09:42:51 -0800

    commons extended

--- src/configmate/common/registry.py
+++ src/configmate/common/registry.py
@@ -1,15 +1,34 @@
 import collections
-from typing import Callable, Deque, Generic, NoReturn, Tuple, TypeVar
+from typing import Callable, Deque, Dict, Generic, NoReturn, Tuple, TypeVar
 
-from configmate.common import exceptions, types
+from configmate.common import exceptions
 
 T_contra = TypeVar("T_contra", contravariant=True)
 U = TypeVar("U")
 
 
+class DictRegistryMixin(Generic[T_contra, U]):
+    _registry: Dict[T_contra, U]
+
+    def __init_subclass__(cls, *args, **kwargs) -> None:
+        cls._registry = {}
+        return super().__init_subclass__(*args, **kwargs)
+
+    @classmethod
+    def lookup(cls, key: T_contra) -> U:
+        """Retrieves first triggered strategy or raises `~exceptions.NoApplicableStrategy`."""
+        if key in cls._registry:
+            return cls._registry[key]
+        _raise_missing(cls, key)
+
+    @classmethod
+    def register(cls, key: T_contra, value: U) -> None:
+        """Inserts a new strategy at the given rank (position in queue)."""
+        cls._registry[key] = value
+
+
 class StrategyRegistryMixin(Generic[T_contra, U]):
     _registry: Deque[Tuple[Callable[[T_contra], bool], U]]
-    _sentinel = types.Sentinel()  # trigger for no applicable strategy
 
     def __init_subclass__(cls, *args, **kwargs) -> None:
         cls._registry = collections.deque()
@@ -17,10 +36,9 @@ class StrategyRegistryMixin(Generic[T_contra, U]):
 
     def get_first_match(self, key: T_contra) -> U:
         """Retrieves first triggered strategy or raises `~exceptions.NoApplicableStrategy`."""
-        methods = (entry for trigger, entry in self._registry if trigger(key))
-        if isinstance(first_match := next(methods, self._sentinel), types.Sentinel):
-            self._raise_missing(key)
-        return first_match
+        for entry in (entry for trigger, entry in self._registry if trigger(key)):
+            return entry
+        _raise_missing(self, key)
 
     @classmethod
     def register(
@@ -29,8 +47,9 @@ class StrategyRegistryMixin(Generic[T_contra, U]):
         """Inserts a new strategy at the given rank (position in queue)."""
         cls._registry.insert(rank, (trigger, entry))
 
-    def _raise_missing(self, unfound_key: T_contra) -> NoReturn:
-        raise exceptions.NoApplicableStrategy(
-            f"Missing strategy for {unfound_key}, please register one."
-            f"\n\tAvailable strategies: "  # TODO describe _registry
-        )
+
+def _raise_missing(self: object, unfound_key) -> NoReturn:
+    raise exceptions.NoApplicableStrategy(
+        f"Missing strategy for {unfound_key}, please register one."
+        f"\n\tAvailable strategies: "  # TODO describe _registry
+    )
--- src/configmate/common/types.py
+++ src/configmate/common/types.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any, Callable, Generic, Protocol, TypeVar, Union
+from typing import Any, Callable, Generic, Mapping, Protocol, TypeVar, Union
 
 T = TypeVar("T")
 U = TypeVar("U")
@@ -7,12 +7,20 @@ T_co = TypeVar("T_co", covariant=True)
 T_contra = TypeVar("T_contra", contravariant=True)
 FilePath = Union[str, os.PathLike]
 
+InterpolationStrategy = Callable[[str], str]
 ParsingStrategy = Callable[[str], T]
 ValidationStrategy = Callable[[Any], T]
 
+RecursiveMapping = Mapping[str, Union[T_co, "RecursiveMapping"]]
+
 # fmt: off
 class Inferred: 'Marks a value that is inferred during runtime.'
-class Sentinel: 'Used to catch exhausted iterators.'
+class Sentinel:
+    'Used to catch exhausted iterators.'
+    def __iter__(self) -> 'Sentinel': return self # pylint: disable=multiple-statements
+    def __next__(self) -> 'Sentinel': return self # pylint: disable=multiple-statements
+SENTINEL = Sentinel()
+
 class Unset: 'Used to mark a value that will be set.'
 # fmt: on
 
@@ -22,12 +30,4 @@ class RegistryProtocol(Protocol, Generic[T_contra, T_co]):
 
     # fmt: off
     def __getitem__(self, key: T_contra) -> T_co: ...
-    # fmt: on
-
-
-class ValidatorProtocol(Protocol, Generic[T_co]):
-    """A validator that can be used to validate a value."""
-
-    # fmt: off
-    def __call__(self, value: Any) -> T_co: ...
     # fmt: on
