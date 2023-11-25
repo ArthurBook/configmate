485d9782e489a00e9ad19c98a5e689c3ec99c11b
Arthur <atte.book@gmail.com>
2023-11-25 09:43:25 -0800

    rewrote interpolation

--- src/configmate/interpolation/__init__.py
+++
@@ -1,9 +0,0 @@
-from configmate.interpolation.env_var_interpolation import (
-    EnvInterpolator,
-    MissingPolicy,
-)
-from configmate.interpolation.interpolator_factory import (
-    InterpolationSpec,
-    InterpolatorFactoryRegistry,
-    make_interpolator,
-)
