23dbc21774960a901e386ac050ec9305e38c1df6
Arthur <atte.book@gmail.com>
2023-12-03 11:41:49 -0800

    rewritten cli readers

--- src/configmate/overlay_suppliers/__init__.py
+++
@@ -1,8 +0,0 @@
-from configmate.overlay_suppliers.cli_overlay_factories import (
-    OverlayArgOptions,
-    OverlayArgSpec,
-    OverlayFileOptions,
-    OverlayFileSpec,
-    construct_arg_supplier,
-    construct_file_supplier,
-)
