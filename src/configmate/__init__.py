7de26a7c2fa469c1ad97c3be6dacd8185fd627d4
Arthur <atte.book@gmail.com>
2024-01-20 10:04:58 -0800

    working microlibs!

--- src/configmate/__init__.py
+++ src/configmate/__init__.py
@@ -2,3 +2,20 @@
 TODO: docs
 """
 from configmate.core.functions import get_config, configure
+
+###
+# Loads all plugins from the configmate_plugins package
+# These are optional dependencies that extend configmate.
+# You can find them in the plugins/ directory.
+###
+try:
+    import configmate_plugins  # type: ignore
+except ImportError:
+    pass  # no plugins
+else:
+    import importlib
+    import pkgutil
+
+    ## Load all plugins
+    for plugin in pkgutil.iter_modules(configmate_plugins.__path__):
+        importlib.import_module(f"{configmate_plugins.__name__}.{plugin.name}")
