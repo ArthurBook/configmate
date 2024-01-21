5c83dac06f38cfa7f5e568a331942915a59c8b03
Arthur <atte.book@gmail.com>
2024-01-21 12:04:34 -0800

    pydantic extension

---
+++ plugins/configmate-pydantic/configmate_plugins/pydantic_validator/__init__.py
@@ -0,0 +1,7 @@
+""" Pydantic Validator Plugin
+extends the configmate library with a validator that uses pydantic to validate input
+"""
+from configmate_plugins.pydantic_validator.pydantic_validator import (
+    PydanticValidator,
+    validate_with_pydantic,
+)
