00147ace33b650fa7b72436eba45f7ab6516d7cb
Arthur <atte.book@gmail.com>
2024-02-16 16:41:38 -0800

    Simplified test

--- tests/test_aggregation.py
+++ tests/test_aggregation.py
@@ -3,16 +3,10 @@ from configmate.base import exceptions, operators
 from configmate.components import aggregators
 
 
-@pytest.fixture
-def context() -> operators.Context:
-    return operators.Context()
-
-
-def test_function_aggregator_aggregate_with_function(context: operators.Context):
+def test_function_aggregator_aggregate_with_function():
     # Test that FunctionAggregator correctly applies a function over an iterable
-    func = sum
-    aggregator = aggregators.FunctionAggregator(func)
-    assert aggregator._transform(context, [1, 2, 3]) == 6
+    aggregator = aggregators.FunctionAggregator(sum)
+    assert aggregator([1, 2, 3]) == 6
 
 
 @pytest.mark.parametrize(
@@ -23,10 +17,10 @@ def test_function_aggregator_aggregate_with_function(context: operators.Context)
     ],
 )
 def test_inferred_aggregator_aggregate_sequences(
-    input_data: list[list[int]], expected_output: list[int], context: operators.Context
+    input_data: list[list[int]], expected_output: list[int]
 ):
     aggregator = aggregators.InferredAggregator()
-    assert aggregator._transform(context, input_data) == expected_output
+    assert aggregator(input_data) == expected_output
 
 
 @pytest.mark.parametrize(
@@ -37,16 +31,16 @@ def test_inferred_aggregator_aggregate_sequences(
     ],
 )
 def test_inferred_aggregator_aggregate_mappings(
-    input_data: list[dict], expected_output: dict, context: operators.Context
+    input_data: list[dict], expected_output: dict
 ) -> None:
     aggregator = aggregators.InferredAggregator()
-    assert aggregator._transform(context, input_data) == expected_output
+    assert aggregator(input_data) == expected_output
 
 
-def test_inferred_aggregator_aggregate_mixed_types(context: operators.Context) -> None:
+def test_inferred_aggregator_aggregate_mixed_types() -> None:
     aggregator = aggregators.InferredAggregator()
     with pytest.raises(exceptions.AggregationFailure):
-        aggregator._transform(context, [1, "a"])
+        aggregator([1, "a"])
 
 
 def test_aggregator_factory_build_function_aggregator():
